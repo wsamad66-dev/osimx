402f4dc0e92f4de19a2a57a389365886b6dd15ff
wsamad66-dev <ouassimsamad8@gmail.com>
2025-10-14 23:04:54 +0200

    fix: Corriger import structureTool dans sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
+import { structure as structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemas'
 import { structure } from './sanity/structure'
