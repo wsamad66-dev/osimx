e01eb06bf5658226dc40b0320fd38b316c658618
wsamad66-dev <ouassimsamad8@gmail.com>
2025-10-16 21:39:50 +0200

    fix: Corriger import structureTool (utiliser import nommé correct)

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -2,7 +2,7 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import { defineConfig } from 'sanity'
-import { structure as structureTool } from 'sanity/structure'
+import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 
 // Import schemas
